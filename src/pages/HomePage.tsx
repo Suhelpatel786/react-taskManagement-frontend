514a0c32849432e95290f9aaed3939a10cc1f8ce
Suhel Patel <suhel.softcolon@gmail.com>
2024-03-26 12:16:02 +0530

    bug fixed

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,69 @@
 import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import { Typewriter } from "react-simple-typewriter";
+import { colors } from "../constants";
+import { color } from "framer-motion";
 
 const HomePage = () => {
-  return <div>Home Page</div>;
+  const handleType = (count: number) => {
+    // access word count number
+    console.log(count);
+  };
+
+  const handleDone = () => {
+    console.log(`Done after 5 loops!`);
+  };
+
+  return (
+    <Box>
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        width={"100vw"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Typography
+          variant="h3"
+          sx={{
+            pt: "5rem",
+            margin: "auto 0",
+            fontWeight: "700",
+            color: "black",
+            lineHeight: "5rem",
+          }}
+          style={{ paddingTop: "5rem", margin: "auto 0", fontWeight: "normal" }}
+        >
+          Welcome to our task management platform
+        </Typography>
+        <Typography variant="h2" color={colors.MainDarkColor}>
+          <Typewriter
+            cursorBlinking={false}
+            cursorColor={colors.MainDarkColor}
+            words={[
+              "Stay organized",
+              "Boost productivity",
+              "Take control",
+              "Achieve goals",
+              "Streamline workflow",
+              "Never miss deadlines",
+              "Seamless collaboration",
+              "Transform tasks",
+              "Empower success",
+            ]}
+            loop={5}
+            cursor
+            cursorStyle="|"
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+            onLoopDone={handleDone}
+            onType={handleType}
+          />
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default HomePage;
