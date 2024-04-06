cc5c22d66712a9c378eab235dcf91b128200fba1
Suhel Patel <suhel.softcolon@gmail.com>
2024-04-07 00:59:46 +0530

    profile page design*

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,7 +1,238 @@
-import React from "react";
+import { Box, Icon, Typography } from "@mui/material";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import { useState } from "react";
+import { colors } from "../constants";
+import { AiOutlineMail } from "react-icons/ai";
+import { CiUser } from "react-icons/ci";
+import { RiLockPasswordLine } from "react-icons/ri";
 
 const ProfilePage = () => {
-  return <div>Profile Page</div>;
+  const [isUpdate, setIsUpdate] = useState<boolean>(false);
+
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  const ProfileSchema = Yup.object({
+    name: Yup.string()
+      .required("Please Enter your name")
+      .min(3, "Must 3 character or more")
+      .max(20, "Must 20 character or less"),
+    email: Yup.string()
+      .required("Please enter your Email")
+      .matches(emailPattern, "Invalid email format"),
+    password: Yup.string()
+      .required("Please enter your Password")
+      .min(6, "Must be 6 characters or more")
+      .max(8, "Must be 8 characters or less"),
+  });
+
+  const handleUpdate = () => {
+    console.log(values);
+  };
+
+  const { errors, values, touched, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      name: "Suhel Patel",
+      email: "suhel@gmail.com",
+      password: "suhel@123",
+    },
+    validationSchema: ProfileSchema,
+    onSubmit: (values) => handleUpdate(),
+  });
+
+  function printStars(str: any) {
+    const length = str.length;
+    const stars = "*".repeat(length);
+    return stars;
+  }
+
+  return (
+    <Box mt={"5rem"}>
+      <Box
+        width={"100%"}
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Box
+          width={"50%"}
+          py={"1rem"}
+          sx={{
+            backgroundColor: isUpdate ? "transparent" : colors.MainDarkColor,
+            borderRadius: "10px",
+            boxShadow: !isUpdate
+              ? "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;"
+              : "",
+          }}
+        >
+          <Box width={"100%"} display={"flex"} justifyContent={"flex-end"}>
+            <button
+              className="login-button"
+              style={{ marginRight: ".5rem" }}
+              type="submit"
+              onClick={() => setIsUpdate(!isUpdate)}
+            >
+              Edit
+            </button>
+          </Box>
+
+          {isUpdate ? (
+            <Box width={"100%"} mt={"2rem"}>
+              <form onSubmit={handleSubmit}>
+                <Box
+                  width={"100%"}
+                  display={"flex"}
+                  flexDirection={"column"}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                >
+                  <input
+                    // type="email"
+                    className="task-input"
+                    name="name"
+                    value={values.name}
+                    style={{ marginBottom: "2rem" }}
+                    autoComplete="off"
+                    onChange={handleChange}
+                    placeholder="Enter name"
+                  />
+
+                  {touched.name && errors.name ? (
+                    <p style={{ marginTop: ".5rem" }} className="error-p">
+                      {errors.name}
+                    </p>
+                  ) : null}
+
+                  <input
+                    // type="email"
+                    style={{ marginBottom: "2rem" }}
+                    className="task-input"
+                    name="email"
+                    value={values.email}
+                    autoComplete="off"
+                    onChange={handleChange}
+                    placeholder="Enter email"
+                  />
+
+                  {touched.email && errors.email ? (
+                    <p style={{ marginTop: ".5rem" }} className="error-p">
+                      {errors.email}
+                    </p>
+                  ) : null}
+
+                  <input
+                    style={{ marginBottom: "2rem" }}
+                    className="task-input"
+                    name="password"
+                    value={values.password}
+                    autoComplete="off"
+                    onChange={handleChange}
+                    placeholder="Enter Passoword"
+                  />
+
+                  {touched.email && errors.email ? (
+                    <p style={{ marginTop: ".5rem" }} className="error-p">
+                      {errors.email}
+                    </p>
+                  ) : null}
+
+                  <button
+                    className="login-button"
+                    style={{ marginTop: "1rem" }}
+                    type="submit"
+                  >
+                    Update
+                  </button>
+                </Box>
+              </form>
+            </Box>
+          ) : (
+            <Box>
+              <Box
+                display={"flex"}
+                sx={{
+                  px: "2rem",
+                  py: ".5rem",
+                  mt: "2rem",
+                  borderRadius: "5px",
+                }}
+                alignItems={"center"}
+                justifyContent={"center"}
+                gap={"1rem"}
+              >
+                <Icon
+                  sx={{ fontSize: "35px", fontWeight: 600, color: "white" }}
+                >
+                  <CiUser />
+                </Icon>
+                <Typography
+                  sx={{
+                    color: "white",
+                    fontSize: "25px",
+                    fontWeight: "500",
+                  }}
+                >
+                  {values.name}
+                </Typography>
+              </Box>
+              <Box
+                display={"flex"}
+                sx={{
+                  px: "2rem",
+                  py: ".5rem",
+                  borderRadius: "5px",
+                }}
+                alignItems={"center"}
+                justifyContent={"center"}
+                gap={"1rem"}
+              >
+                <Icon
+                  sx={{ fontSize: "35px", fontWeight: 600, color: "white" }}
+                >
+                  <AiOutlineMail />
+                </Icon>
+                <Typography
+                  sx={{
+                    color: "white",
+                    fontSize: "25px",
+                    fontWeight: "500",
+                  }}
+                >
+                  {values.email}
+                </Typography>
+              </Box>
+              <Box
+                display={"flex"}
+                sx={{
+                  px: "2rem",
+                  py: ".5rem",
+                  borderRadius: "5px",
+                }}
+                alignItems={"center"}
+                justifyContent={"center"}
+                gap={"1rem"}
+              >
+                <Icon
+                  sx={{ fontSize: "35px", fontWeight: 600, color: "white" }}
+                >
+                  <RiLockPasswordLine />
+                </Icon>
+                <Typography
+                  sx={{
+                    color: "white",
+                    fontSize: "25px",
+                    fontWeight: "500",
+                  }}
+                >
+                  {printStars(values.password)}
+                </Typography>
+              </Box>
+            </Box>
+          )}
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default ProfilePage;
